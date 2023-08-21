b8669f7ef6e5718f67ae922d3f0f35f53ff91df2
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-08-21 00:24:03 -0400

    update tree_plot

--- formatTreeTxt2Newick.py
+++ formatTreeTxt2Newick.py
@@ -1,3 +1,3 @@
 from ete3 import Tree
 t = Tree('sp_tree.txt', format = 1)
-t.write(outfile = 'sp_tree.newick')
+t.write(format = 1, outfile = 'sp_tree.newick')
---
+++ plot_tree.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+graphlan.py sp_tree.newick sp_tree.png --size 100
