e186d2f7c0feff85ae9140b07f85c2bfa1fde201
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-08-27 17:08:03 -0400

    update code for tree

--- tree_code1.R
+++ tree_code1.R
@@ -6,12 +6,27 @@ library(tidyr)
 library(ggplot2)
 library(ggpp)
 library(deeptime)
-
+library(ggnewscale)
 
 t <- read.tree('tree_sp.newick')
-# tp <- ggtree(t, layout = 'circular')
-
 data <- read_csv('gram_stain_prediction_sp.csv', show_col_types = FALSE)
+
+sp_data <- data |> 
+    filter(Rank == 'species') |> 
+    mutate(
+        NCBI_ID = paste0(sub('^(\\w).*$', '\\1', Rank), '__', NCBI_ID)
+    ) |> 
+    select(NCBI_ID, Attribute, Score) |> 
+    distinct() |> 
+    pivot_wider(
+        names_from = 'Attribute', values_from = 'Score'
+    ) |> 
+    as.data.frame() |> 
+    rename(node = NCBI_ID) |> 
+    relocate(node, .after = last_col()) |> 
+    filter(node %in% t$tip.label) |> 
+    tibble::column_to_rownames(var = 'node')
+
 data <- data |> 
     filter(! Rank %in% c('species', 'strain')) |> 
     mutate(
@@ -35,7 +50,6 @@ data <- data[!is.na(rowSums(data)),]
 data$node <- as.integer(rownames(data))
 
 myPies <- nodepie(data, cols = 1:3)
-# t2 <- inset(tree_view = tp, insets = myPies[1:3], width = 0.03, height = 0.03)
 
 df <- tibble::tibble(node=as.numeric(data$node), pies=myPies)
 p1 <- ggtree(t, layout = 'circular', size = 0.025)
@@ -47,7 +61,10 @@ p3 <- p2 +
     vp.width = 0.01, vp.height = 0.01, 
     hjust = 0.5, vjust = 0.5
 ) 
+
 ggsave(
     filename = 'test_tree_plot.png', plot = p3, width = 10, height = 10,
     units = 'in', dpi = 300
 )
+
+
