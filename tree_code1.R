688537e281d8074110df343f7af2515c1b5877ba
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-08-28 09:02:16 -0400

    update tree code

--- tree_code1.R
+++ tree_code1.R
@@ -12,6 +12,7 @@ t <- read.tree('tree_sp.newick')
 data <- read_csv('gram_stain_prediction_sp.csv', show_col_types = FALSE)
 p <- ggtree(t, layout = 'circular', size = 0.025)
 
+# Add heatmap for external rings ------------------------------------------
 tip_data <- data |> 
     filter(Rank == 'species') |> 
     mutate(
@@ -27,10 +28,22 @@ tip_data <- data |>
     relocate(node, .after = last_col()) |> 
     filter(node %in% t$tip.label) |> 
     tibble::column_to_rownames(var = 'node')
+tip_data <- tip_data[t$tip.label,]
+tip_data <- tip_data[!is.na(rowSums(tip_data)),]
+
+p_heatmap <- 
+    
+    gheatmap(p, data = tip_data)
 
 
 
+ggsave(
+    filename = 'test_tree_plot.png', plot = p_heatmap, width = 10, height = 10,
+    units = 'in', dpi = 300
+)
 
+
+# Add inset pies ----------------------------------------------------------
 node_data <- data |> 
     filter(! Rank %in% c('species', 'strain')) |> 
     mutate(
@@ -51,12 +64,10 @@ rownames(node_data) <- length(t$tip.label) + 1:t$Nnode
 node_data <- node_data[!is.na(rowSums(node_data)),]
 node_data$node <- as.integer(rownames(node_data))
 
-
-
 pies <- nodepie(node_data, cols = 1:3)
 df <- tibble::tibble(node = as.numeric(node_data$node), pies = pies) # A tibble of pies (insets)
-p <- p %<+% df
-p_with_pies <- p + 
+p_with_pies <- p_heatmap %<+% 
+    df +
     ggpp::geom_plot(
         data = td_filter(!isTip), 
         mapping = aes(x = x, y = y, label = pies), 
@@ -64,6 +75,7 @@ p_with_pies <- p +
         hjust = 0.5, vjust = 0.5
 ) 
 
+# Save plot ---------------------------------------------------------------
 ggsave(
     filename = 'test_tree_plot.png', plot = p_with_pies, width = 10, height = 10,
     units = 'in', dpi = 300
