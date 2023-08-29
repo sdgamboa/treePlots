5dc75949a505e8518fa0ea6a6b8cf44328641b8a
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-08-29 02:12:25 -0400

    update code and suobset plot

--- tree_code1b.R
+++ tree_code1b.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(ggpp)
 library(deeptime)
 library(ggnewscale)
+library(purrr)
 
 t <- read.tree('tree_sp.newick')
 data <- read_csv('gram_stain_prediction_sp.csv', show_col_types = FALSE)
@@ -125,10 +126,10 @@ p_with_pies <- p_heatmap %<+%
 )
 
 # Save plot ---------------------------------------------------------------
-ggsave(
-    filename = 'test_tree_plot.png', plot = p_with_pies, width = 10, height = 10,
-    units = 'in', dpi = 150 
-)
+# ggsave(
+#     filename = 'test_tree_plot.png', plot = p_with_pies, width = 10, height = 10,
+#     units = 'in', dpi = 150 
+# )
 
 
 # Prune tree --------------------------------------------------------------
@@ -138,27 +139,51 @@ pruned_t <- drop.tip(t, t$tip.label[-match(keep_tips, t$tip.label)])
 subset_data_holdout <- data_holdout[keep_tips,] # Only tip data from holdouts
 subset_tip_data <- tip_data[keep_tips,] # includes tip data from predicted values for holdouts
 
-p2 <- ggtree(pruned_t, layout = 'circular', size = 0.02)
+tips <- sub('^\\w__(\\d+)$', '\\1', pruned_t$tip.label)
+tip_taxnames <- taxizedb::taxid2name(tips, db = 'ncbi')
+# names(tip_taxnames) <- tips
+# pos <- which(is.na(tip_taxnames))
+# tip_taxnames[pos] <- tips[pos]
+
+pruned_t$tip.label <- tip_taxnames
+rownames(subset_data_holdout) <- tip_taxnames
+rownames(subset_tip_data) <- tip_taxnames
+
+node_ids <- sub('^\\w__(\\d+)$', '\\1', pruned_t$node.label)
+original_node_ids <- pruned_t$node.label
+node_taxnames <- taxizedb::taxid2name(node_ids[2:length(node_ids)], db = 'ncbi')
+node_taxnames <- c(NA, node_taxnames)
+names(node_taxnames) <- node_ids
+node_pos <- which(is.na(node_taxnames))
+node_taxnames[node_pos] <- node_ids[node_pos]
+pruned_t$node.label <- node_taxnames
+
+p2 <- pruned_t |> 
+    ggtree(layout = 'circular', size = 0.02) +
+    geom_tiplab(size = 0.8) +
+    geom_nodelab(size = 0.8)
+
+
 p_heatmap2_ <- p2 |> 
     gheatmap(
-        data = subset_tip_data, offset = 0.1, width = 0.1, 
+        data = subset_tip_data, offset = 0.1 + 3, width = 0.1, 
         # colnames_angle = 90, colnames_offset_y = .25,,
         colnames = FALSE,
         # legend_title = 'Predicted score',
         color = NA
         # low = 'white', high = 'firebrick'
     ) +
-    scale_fill_viridis_c(option = 'D', name = 'Score', na.value = 'white')
+    scale_fill_viridis_c(option = 'D', name = 'Prediced score', na.value = 'white')
 
 p_heatmap2 <- p_heatmap2_ + new_scale_fill()
 p_heatmap2 <- p_heatmap2 |> 
     gheatmap(
-        data = subset_data_holdout, offset = 1, width = 0.1, 
+        data = subset_data_holdout, offset = 1 + 3, width = 0.1, 
         # colnames_angle = 90, colnames_offset_y = .25,,
         colnames = FALSE,
         color = NA
     ) +
-    scale_fill_viridis_c(option = 'D', name = 'Score', na.value = 'white')
+    scale_fill_viridis_c(option = 'C', name = 'Holdout score', na.value = 'white')
     # scale_fill_gradient(
     #     name = 'Holdout score',
     #     low = 'white', high = 'dodgerblue4', na.value = 'white', 
@@ -179,7 +204,10 @@ node_data2 <- data |>
     relocate(node, .after = last_col()) |> 
     filter(node %in% t$node.label) |> 
     tibble::column_to_rownames(var = 'node') 
-node_data2 <- node_data2[pruned_t$node.label,] # I do this to make sure that I have the same order for nodes
+# node_data2 <- node_data2[pruned_t$node.label,] # I do this to make sure that I have the same order for nodes
+
+
+node_data2 <- node_data2[original_node_ids,] # I do this to make sure that I have the same order for nodes
 rownames(node_data2) <- length(pruned_t$tip.label) + 1:pruned_t$Nnode
 node_data2[is.na(node_data2)] <- 0
 
@@ -189,7 +217,7 @@ colnames(node_data2) <- gsub(' ', '_', colnames(node_data2))
 myColors <- c('red', 'blue', 'yellow')
 names(myColors) <- colnames(node_data2)[1:3]
 
-pies2 <- nodepie(node_data2, cols = 1:3)
+pies2 <- nodepie(node_data2, cols = 1:3, alpha = 0.3)
 pies2 <- lapply(pies2, function(g) g + scale_fill_manual(values = myColors))
 
 df2 <- tibble::tibble(node = as.numeric(node_data2$node), pies = pies2) # A tibble of pies (insets)
@@ -207,3 +235,8 @@ ggsave(
     filename = 'test_tree_plot_subset.png', plot = p_with_pies2, width = 10, height = 10,
     units = 'in', dpi = 300 
 )
+
+# ggsave(
+#     filename = 'p2.png', plot = p2, width = 10, height = 10,
+#     units = 'in', dpi = 300 
+# )
