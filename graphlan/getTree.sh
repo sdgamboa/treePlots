37706da2b1ec47f2b1444252014e89768ff71af8
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-11-30 16:24:03 -0500

    add scripf for retrieving tree data

---
+++ graphlan/getTree.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wget https://raw.githubusercontent.com/waldronlab/taxPPro/main/inst/extdata/LTP_all_08_2023.newick
+wget https://raw.githubusercontent.com/waldronlab/taxPPro/main/inst/extdata/LTP_all_08_2023.node_data
+wget https://raw.githubusercontent.com/waldronlab/taxPPro/main/inst/extdata/LTP_all_08_2023.tip_data
